8296d68a222357115901eeca10ede9ed10d7498b
David Jacobo <djacobos@yahoo.com>
2015-12-12 15:07:59 -0600

    Included layout to support material design

--- AutoMaintenance/Droid/MainActivity.cs
+++ AutoMaintenance/Droid/MainActivity.cs
@@ -7,18 +7,20 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Xamarin.Forms.Platform.Android;
 
 namespace AutoMaintenance.Droid
 {
 	[Activity (Label = "AutoMaintenance.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	public class MainActivity : FormsAppCompatActivity
 	{
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 
 			global::Xamarin.Forms.Forms.Init (this, bundle);
-
+			FormsAppCompatActivity.ToolbarResource = Resource.Layout.toolbar;
+			FormsAppCompatActivity.TabLayoutResource = Resource.Layout.tabs;
 			LoadApplication (new App ());
 		}
 	}
