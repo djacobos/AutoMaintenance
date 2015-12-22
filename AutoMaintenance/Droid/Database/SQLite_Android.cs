e44be80f6d2b00c2b5d25959394920aa9e3d03a8
David Jacobo <djacobos@yahoo.com>
2015-12-21 23:27:57 -0600

    Add sql-lite package

---
+++ AutoMaintenance/Droid/Database/SQLite_Android.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using AutoMaintenance.Droid;
+using System.IO;
+using SQLite;
+
+[assembly: Dependency (typeof(SQLite_Android))]
+namespace AutoMaintenance.Droid
+{
+	public class SQLite_Android : ISQLite
+	{
+		#region ISQLite implementation
+
+		public SQLiteConnection GetConnection ()
+		{
+			var sqliteFilename = "AutoMaintenance.db3";
+			var documentsPath = Environment.GetFolderPath (Environment.SpecialFolder.Personal);
+			var libraryPath = Path.Combine (documentsPath, "..", "Library");
+			var path = Path.Combine (libraryPath, sqliteFilename);
+
+			var connection = new SQLiteConnection (path);
+
+			return connection;
+		}
+
+		#endregion
+	}
+}
+
