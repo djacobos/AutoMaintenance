2412704db4b6b2bb2a7065e4d429d7e3f346cd97
David Jacobo <djacobos@yahoo.com>
2015-12-21 23:20:51 -0600

    Add login page

--- AutoMaintenance/AutoMaintenance/View/LoginPage.xaml.cs
+++ AutoMaintenance/AutoMaintenance/View/LoginPage.xaml.cs
@@ -8,13 +8,33 @@ namespace AutoMaintenance
 	public partial class LoginPage : ContentPage
 	{
 		public LoginPage ()
-		{
-			InitializeComponent ();
+		{			
+			InitializeComponent ();		
 		}
+
 
 		public void OnClick_Login(object o, EventArgs e)
 		{
-			Navigation.PushAsync (new DashboardPage());
+			bool userPassCombination = true;
+
+			if (userName.Text != "david")
+				userPassCombination = false;
+
+			if (password.Text != "david")
+				userPassCombination = false;
+
+			if (userPassCombination) {
+				Navigation.PushAsync (new DashboardPage());
+			}
+
+
+			// Notify user of invalid credentials
+			errorMessage.Text = "Error: Invalid credentials, try again.";
+		}
+
+		public void OnClick_Register(object o, EventArgs e)
+		{
+			Navigation.PushAsync (new RegisterPage());
 		}
 	}
 }
